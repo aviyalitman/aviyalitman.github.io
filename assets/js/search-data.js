42cf33e332046dfaff6660481c428a43f070bffa
aviyalitman <52050744+aviyalitman@users.noreply.github.com>
2025-05-21 20:18:34 +0000

    Deploying to gh-pages from @ aviyalitman/aviyalitman.github.io@527f2003fc02960d4bc245c89c337117aa7aa5ae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -454,13 +454,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/aviya-litman", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
