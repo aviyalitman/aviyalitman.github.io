f930708fb7392f97186825e9d2f72d6a0c3c30ea
aviyalitman <52050744+aviyalitman@users.noreply.github.com>
2025-06-15 22:37:20 +0000

    Deploying to gh-pages from @ aviyalitman/aviyalitman.github.io@9fe04d4db07939fa826e166ef4a4ee4ba6f09ebc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -376,16 +376,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-my-paper-was-accepted-to-nature-genetics-coming-soon-tada",
+          title: 'My paper was accepted to Nature Genetics! Coming soon! :tada:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
